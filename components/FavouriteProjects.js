1b56919ef52172061c10f86ad3290de653b899f3
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-04-28 23:05:00 +0530

    added react icons

--- components/FavouriteProjects.js
+++ components/FavouriteProjects.js
@@ -47,7 +47,7 @@ export default function FavouriteProjects() {
                     alt="portfolio"
                     className="transform hover:scale-125 transition duration-2000 ease-out"
                   />
-                  <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+                  <h1 className="absolute top-10 left-10 text-gray-100 font-bold text-xl bg-blue-500 rounded-md px-2">
                     {project.title}
                   </h1>
                   <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
