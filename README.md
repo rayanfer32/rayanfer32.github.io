ddaf288882d57c91534b71cccc8069d943533cba
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-04-28 20:31:52 +0530

    ✨ update new portfolio design

---
+++ README.md
@@ -0,0 +1,3 @@
+## Portfolio Website For Developers 💯
+
+
---
+++ constants/data.js
@@ -0,0 +1,118 @@
+const userData = {
+    githubUsername: "rayanfer32",
+    name: "Rayan Fernandes",
+    description: "Developer, Designer, and Freelancer",
+    designation: "Full-Stack Developer",
+    avatarUrl: "/profile_pic.png",
+    email: "rayanfernandes9@gmail.com",
+    phone: "+91 8722277308",
+    address: "Mangalore, Karnataka, India.",
+    domainURL: "https://rayanfer32.github.io",
+    traits: [
+        ["Coder", "#F59E0B"],
+        ["Designer", "#84CC16"],
+        ["Creator", "#10B981"],
+        ["Innovator", "#3B82F6"],
+    ],
+    projects: [{
+            title: "Nexus Explorer Next",
+            link: "https://explorer.nexus.io",
+            imgUrl: "/nexus_explorer.jpg",
+        },
+        {
+            title: "Demuxx Bot",
+            link: "https://t.me/demuxxbot",
+            imgUrl: "/demuxx.png",
+        },
+        {
+            title: "Tgindex Pro",
+            link: "https://github.com/rayanfer32/TgindexPro",
+            imgUrl: "/tgindex.jpg",
+        },
+        {
+            title: "Dicto-Writer",
+            link: "https://github.com/hackerminds/DictoWriter",
+            imgUrl: "/dicto_writer.jpg",
+        }
+        // {
+        //     title: "Greentech - Greenhouse Automation",
+        //     link: "https://greentech.rayanfer32.repl.co",
+        //     imgUrl: "/greentech.jpg",
+        // },
+    ],
+    meta: {
+        title: "Rayan Fernandes - Coder. Designer. Freelancer",
+        description: `Building things gets me excited. Get in touch with me to know more.`,
+        image: "/avatar.png",
+        type: "website"
+    },
+    about: {
+        title: "I'm a software developer that loves building products and web applications that impact millions of lives",
+        description: [
+            `I've been exploring and coding since I was 18.`,
+            `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+            `I've been coding in Python, React, Node.js, Express, MySQL, Bootstrap, .NET, C, C++, C#, Java, Assembly language etc to name a few. Although I barely remember the syntax of all the languages now though, I consider myself a Ninja developer.`,
+        ],
+        currentProject: "Nexus Explorer",
+        currentProjectUrl: "https://explorer.nexus.io",
+        techStackIcons: ["https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png",
+            "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/java/java.png",
+            "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/typescript/typescript.png",
+            "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png",
+            "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png",
+            "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/angular/angular.png",
+            "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/vue/vue.png",
+            "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/git/git.png",
+            "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png",
+            "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/firebase/firebase.png",
+            "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mysql/mysql.png",
+            "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongodb/mongodb.png"
+        ]
+    },
+    experience: [{
+            title: "Full Stack Developer",
+            company: "Nexus.io",
+            year: "2022",
+            companyLink: "https://explorer.nexus.io",
+            desc: "Developed The Nexus Explorer using next.js and python, with figma design tool.",
+        },
+        {
+            title: "Software Developer",
+            company: "Infosys",
+            year: "2021",
+            companyLink: "https://infosys.com",
+            desc: "Systems Engineer, dealing with some of the biggest projects in the industry.",
+        },
+        {
+            title: "Internship",
+            company: "Fiabilite.inc",
+            year: "2020",
+            companyLink: "https://fiabilite.com",
+            desc: "Worked on Embedded IOT and Applications development.",
+        },
+        {
+            title: "Electronics & Communication Engineering",
+            company: "Canara Engineering College",
+            year: "2016-2021",
+            companyLink: "https://canaraengineering.com",
+            desc: "Had a tough time in the electronics and communication engineering department, but I managed to get a 6.7 CGPA.",
+        },
+        {
+            title: "Pre-University College",
+            company: "Padua P.U College, Nanthoor",
+            year: "2014 - 2016",
+            companyLink: "https://paduapucollege.org",
+            desc: "Subjects being Physics, Chemistry, Mathematics, English, Hindi and the most important Computer Science!",
+        },
+    ],
+    resumeUrl: "https://github.com/rayanfer32/Resume/blob/master/Resume8.5.pdf",
+    socialLinks: {
+        instagram: "https://instagram.com/rayanfer32_",
+        twitter: "https://twitter.com/rayanfernandes9",
+        linkedin: "https://linkedin.com/in/rayanfer",
+        github: "https://github.com/rayanfer32",
+        facebook: "https://facebook.com",
+    },
+};
+
+export default userData;
