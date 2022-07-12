e01169c368b7f9cfd3283a282d523989394f744f
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-07-12 23:04:51 +0530

    feat: add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,35 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider defaultTheme="light" attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <GoogleAnalytics />
+      <ThemeProvider defaultTheme="light" attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
 export default MyApp;
+
+export function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-HWCHKD0MJQ`}
+      />
+      <Script strategy="lazyOnload">
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-HWCHKD0MJQ');
+        `}
+      </Script>
+    </>
+  );
+}
