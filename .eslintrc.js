e1eea4e0c96ad6a3da95bae6a13da349498ea008
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2023-04-30 23:55:42 +0530

    conf: set eslint to warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "no-unused-vars": "warn",
+    "react/prop-types": "warn",
+    "react/react-in-jsx-scope": "warn",
+    "react/no-unescaped-entities": "warn",
+    "react/no-unknown-property": "warn",
+    "react/jsx-key": "warn",
+  },
+};
--- components/Projects.js
+++ components/Projects.js
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types"
 import React from "react";
 import userData from "@constants/data";
 
@@ -14,6 +15,7 @@ export default function Projects() {
         <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 py-20 pb-40">
           {userData.projects.map((proj, idx) => (
             <ProjectCard
+              key={proj.title}
               title={proj.title}
               link={proj.link}
               imgUrl={proj.imgUrl}
@@ -52,3 +54,13 @@ const ProjectCard = ({ title, link, desc, imgUrl, number }) => {
     </div>
   );
 };
+
+ProjectCard.propTypes = {
+  desc: PropTypes.any,
+  imgUrl: PropTypes.any,
+  link: PropTypes.any,
+  number: PropTypes.shape({
+    length: PropTypes.number
+  }),
+  title: PropTypes.any
+}
