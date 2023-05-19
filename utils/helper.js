d53747b1231d8a038f1b3d70818be405b144c46b
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2023-05-19 19:32:04 +0530

    feat: added more skills

--- utils/helper.js
+++ utils/helper.js
@@ -5,17 +5,18 @@ function getTopicIcon(item) {
   return `https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/${item}/${item}.png`;
 }
 
-function getFaviconIcon(item){
-  return `https://www.google.com/s2/favicons?sz=64&domain=${item}`
+function getFaviconIcon(item) {
+  return `https://www.google.com/s2/favicons?sz=64&domain=${item}`;
 }
-
 
 export function getMyTechStackIcons() {
   return userData.techStack.map((item) => {
-    if(item.includes('.ico')){
-      return getFaviconIcon(item.split(".ico")[0])
+    if (item.includes("https://")) {
+      return item;
+    } else if (item.includes(".ico")) {
+      return getFaviconIcon(item.split(".ico")[0]);
     }
-    return getTopicIcon(item)
+    return getTopicIcon(item);
   });
 }
 
