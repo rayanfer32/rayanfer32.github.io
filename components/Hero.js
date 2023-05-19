0eaafe335c74d38ec0cfbd2f18c36ef7ada9d50c
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2023-05-19 19:30:53 +0530

    feat: hacked text effect

--- components/Hero.js
+++ components/Hero.js
@@ -2,6 +2,7 @@ import React from "react";
 import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 import { RainbowHighlight } from "./RainbowHighlight";
 import userData from "@constants/data";
+import HackedTextEffect from "./molecules/HackedTextEffect";
 
 export default function Hero() {
   return (
@@ -12,13 +13,12 @@ export default function Hero() {
           {userData.traits.map(([trait, color], index) => (
             <RainbowHighlight key={trait} color={color}>
               <h1 className="p-2 text-2xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
-                {trait}
+                <HackedTextEffect text={trait} />
               </h1>
             </RainbowHighlight>
           ))}
         </RoughNotationGroup>
       </div>
-
       <img src={userData.avatarUrl} alt="avatar" className="lg:h-60 h-32" />
     </div>
   );
---
+++ components/molecules/HackedTextEffect.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef } from "react";
+
+const letters = "abcdefghijklmnopqrstuvwxyz;ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const textEffectOnMouseOver = (refEl, interval) => {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  let ref = refEl.current;
+
+  interval = setInterval(() => {
+    ref.innerText = ref.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return ref.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= ref.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 2;
+  }, 30);
+};
+
+function HackedTextEffect({ text }) {
+  let interval = null;
+  const textRef = useRef();
+
+  function handleMouseOver() {
+    return textEffectOnMouseOver(textRef, interval);
+  }
+
+  useEffect(() => {
+    textRef.current.onmouseover = handleMouseOver;
+    setTimeout(handleMouseOver, 1000);
+  }, []);
+
+  return (
+    <h1 ref={textRef} data-value={text}>
+      {text}
+    </h1>
+  );
+}
+
+export default HackedTextEffect;
