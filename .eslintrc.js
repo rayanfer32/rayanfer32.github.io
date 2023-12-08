33768f7eebeed4e31bf57ebf84195b1f790ca4f3
rayanfer32 <rayanfernandes9@gmail.com>
2023-12-08 22:34:58 +0530

    build: fix linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: ["eslint:recommended", "plugin:react/recommended", 'plugin:@next/next/recommended'],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
