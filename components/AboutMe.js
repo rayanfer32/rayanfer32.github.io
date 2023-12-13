7cba96919abd00e88d7e9a9488efb46f0c97340f
rayanfer32 <rayanfernandes9@gmail.com>
2023-12-14 00:24:16 +0530

    feat: update nextjs to v14

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -53,10 +53,8 @@ export default function AboutMe() {
               <p className="text-lg text-gray-500 mt-4 dark:text-gray-300">
                 I&apos;m looking for a job currently, If you see me as a good
                 fit, check my{" "}
-                <Link href={userData.resumeUrl}>
-                  <a className="text-gray-800 border-b-2 border-gray-800 dark:border-gray-300 font-bold dark:text-gray-300">
+                <Link href={userData.resumeUrl} className="text-gray-800 border-b-2 border-gray-800 dark:border-gray-300 font-bold dark:text-gray-300">
                     CV{" "}
-                  </a>
                 </Link>
                 and I&apos;d love to work for you.
               </p>
--- components/Navbar.js
+++ components/Navbar.js
@@ -31,44 +31,42 @@ export default function Navbar() {
 
         <div className="flex flex-col">
           <Link href="/">
-            <a>
-              <h1 className="font-semibold text-xl dark:text-gray-100">
-                {userData.name}
-              </h1>
-              <p className="text-base font-light text-gray-500 dark:text-gray-300">
-                {userData.designation}
-              </p>
-            </a>
+            <h1 className="font-semibold text-xl dark:text-gray-100">
+              {userData.name}
+            </h1>
+            <p className="text-base font-light text-gray-500 dark:text-gray-300">
+              {userData.designation}
+            </p>
           </Link>
         </div>
 
         <div className="space-x-8 hidden md:block">
           {navbarLinks.map(({ title, path }, index) => (
-            <Link key={title} href={path}>
-              <a
-                className={`text-base ${
-                  router.asPath === path
-                    ? "text-gray-800 font-bold dark:text-gray-400"
-                    : "text-gray-600 dark:text-gray-300 font-normal "
-                }`}
-              >
-                {title}
-                {router.asPath === path && (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-arrow-down inline-block h-3 w-3"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
-                    />
-                  </svg>
-                )}
-              </a>
+            <Link
+              key={title}
+              href={path}
+              className={`text-base ${
+                router.asPath === path
+                  ? "text-gray-800 font-bold dark:text-gray-400"
+                  : "text-gray-600 dark:text-gray-300 font-normal "
+              }`}
+            >
+              {title}
+              {router.asPath === path && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-arrow-down inline-block h-3 w-3"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                  />
+                </svg>
+              )}
             </Link>
           ))}
         </div>
@@ -161,10 +159,12 @@ export default function Navbar() {
       {/* mobile navbar */}
       <div className="space-x-3 flex flex-wrap justify-center md:hidden mt-4">
         {navbarLinks.map(({ title, path }, index) => (
-          <Link key={title} href={path}>
-            <a className="text-base font-normal text-gray-600 dark:text-gray-300">
-              {title}
-            </a>
+          <Link
+            key={title}
+            href={path}
+            className="text-base font-normal text-gray-600 dark:text-gray-300"
+          >
+            {title}
           </Link>
         ))}
       </div>
